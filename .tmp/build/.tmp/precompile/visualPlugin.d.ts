3adf54d27f7302297e60aa1739a40185449809c3
Hayley Stagg <hayley.stagg@neas.nhs.uk>
2023-12-04 09:14:51 +0000

    version 1.0.0.3

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -1,4 +1,4 @@
 import powerbiVisualsApi from "powerbi-visuals-api";
 import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var barCharte052449909264a3487139638376f9e72_DEBUG: IVisualPlugin;
-export default barCharte052449909264a3487139638376f9e72_DEBUG;
+declare var SPCCharte052449909264a3487139638376f9e72: IVisualPlugin;
+export default SPCCharte052449909264a3487139638376f9e72;
